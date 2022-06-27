b72a57c5af0e4e752fd0f5f73928eed2161519d2
j-bruce <bruce.jiams@gmail.com>
2022-06-27 17:35:02 +0900

    첫 커밋

---
+++ src/Speech.js
@@ -0,0 +1,34 @@
+import {useEffect, useRef, useState} from "react";
+import './Speech.scss'
+
+
+export const Speech = props => {
+    const [inputText, setInputText] = useState('')
+    const [charList, setChatList] = useState([])
+    const textInput = useRef()
+
+    useEffect(() => {
+
+    }, [charList])
+
+    function onTextChange(e) {
+        setInputText(e.target.value)
+    }
+
+    function onClickAdd() {
+        const d = [...charList, inputText]
+        setChatList(d)
+        setInputText('')
+        // document.getElementById('myInput').value
+        textInput.current.value = ""
+
+    }
+
+    return <>
+        {charList.map((chat, index) => {
+            return <div class="chatMessage" key={index}>{chat}</div>
+        })}
+        <input type="text" id="myInput" ref={textInput} onChange={onTextChange}></input>
+        <button type="button" onClick={onClickAdd}>Add</button>
+    </>
+}
